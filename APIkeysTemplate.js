080818d3b8b27bc40e62f1f23c489914bbfc27ba
ChrisHanigan <59087814+ChrisHanigan@users.noreply.github.com>
2020-06-09 13:14:14 +1000

    Update APIkeysTemplate.js
    clarity

--- APIkeysTemplate.js
+++ APIkeysTemplate.js
@@ -1,4 +1,4 @@
-//Remove the word Template from this files name.
+//Remove the word Template from this files name so it is just called APIkeys.js
 
 // get Openweather key from https://openweathermap.org/api
 var weatherkey = "Enter Openweather Key Here"
